d1efd971418e896314a029616d7bcdcdced7dc7a
danilogervasio <danilogervasio@172.23.181.203>
2019-06-07 10:21:00 +0200

    Primi tentativi con il json parser

---
+++ src/services/DataDownloader.java
@@ -0,0 +1,75 @@
+package services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class DataDownloader {
+	
+	private String url;
+	
+	public DataDownloader (String url)
+	{
+		this.url=url;
+	}
+	
+	public void startDownloading()
+	{
+					try {
+					
+					URLConnection openConnection = new URL(url).openConnection();
+					openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+					InputStream in = openConnection.getInputStream();
+					
+					 String data = "";
+					 String line = "";
+					 try {
+					   InputStreamReader inR = new InputStreamReader( in );
+					   BufferedReader buf = new BufferedReader( inR );
+					  
+					   while ( ( line = buf.readLine() ) != null ) {
+						   data+= line;
+						   System.out.println( line );
+					   }
+					 } finally {
+					   in.close();
+					 }
+					JSONObject obj = (JSONObject) JSONValue.parseWithException(data); 
+					JSONObject objI = (JSONObject) (obj.get("result"));
+					JSONArray objA = (JSONArray) (objI.get("resources"));
+					
+					for (Object o: objA){
+					    if ( o instanceof JSONObject ) {
+					        JSONObject o1 = (JSONObject)o; 
+					        String format = (String)o1.get("format");
+					        String urlD = (String)o1.get("url");
+					        System.out.println(format + " | " + urlD);
+					        if(format.equals("csv")) {
+					        	download(urlD, "t1.csv");
+					        }
+					    }
+					}
+					System.out.println( "OK" );
+				
+				} catch (Exception e) {
+					e.printStackTrace();
+				}				
+	}
+	
+	public static void download(String url, String fileName) throws Exception {
+	    try (InputStream in = URI.create(url).toURL().openStream()) {
+	        Files.copy(in, Paths.get(fileName));
+	    }
+	}
+
+}
