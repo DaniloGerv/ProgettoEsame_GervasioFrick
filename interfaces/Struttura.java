4fb3f717c64ee68e37a6a800da55174ce679812a
danilogervasio <danilogervasio@danilos-mbp>
2019-06-10 22:10:56 +0200

    Spring si avvia ma si ha un Whitelabel Error Page

--- interfaces/Struttura.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Struttura {
-
-	public abstract String toString();
-
-}
--- src/main/java/com/example/ProgettoEsame_GervasioFrick/ProgettoEsameGervasioFrickApplication.java
+++ src/main/java/com/example/ProgettoEsame_GervasioFrick/ProgettoEsameGervasioFrickApplication.java
@@ -2,20 +2,27 @@ package com.example.ProgettoEsame_GervasioFrick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import services.DataDownloader;
 
 @SpringBootApplication
 public class ProgettoEsameGervasioFrickApplication {
 
+	
 	public static void main(String[] args) {
 		
-		SpringApplication.run(ProgettoEsameGervasioFrickApplication.class, args); //Starting spring service
 		String url="https://www.dati.gov.it/api/3/action/package_show?id=c432300c-687a-47ce-83f0-ddeb1f08cb55";
 		if(args.length == 1)	//if url is given by args
 			url = args[0];
 		DataDownloader data=new DataDownloader(url);	
 		boolean flag=data.readFromJson();
+		if (flag) //if the download is done the spring application is started
+		{
+			SpringApplication.run(ProgettoEsameGervasioFrickApplication.class, args); //Starting spring service
+		}
+		
+		
 		
 	}
 
