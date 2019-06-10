2c26eef3440110cfc643c3ed72d75db85debb269
danilogervasio <danilogervasio@danilos-mbp.homenet.telecomitalia.it>
2019-06-10 20:56:25 +0200

    prova

---
+++ src/main/java/com/example/ProgettoEsame_GervasioFrick/ProgettoEsameGervasioFrickApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoEsame_GervasioFrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoEsameGervasioFrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoEsameGervasioFrickApplication.class, args);
+	}
+
+}
