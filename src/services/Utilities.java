7a18fc216bf636884bcfe4522d5f6ee5cfb49a25
danilogervasio <danilogervasio@172.23.181.203>
2019-06-13 08:11:44 +0200

    Cambiato valore nullInt

--- src/services/Utilities.java
+++ src/services/Utilities.java
@@ -2,7 +2,7 @@ package services;
 
 public class Utilities {
 	
-	public static final int NULLINT=0;
+	public static final int NULLINT=-1;
 	public static final String NULLSTRING="";
 	public static final double NULLDOUBLE=0.0;
 	
