6fed3fe1cb897def5e2d856f342233658f38df93
danilogervasio <danilogervasio@172.23.181.203>
2019-06-20 08:36:15 +0200

    prova

---
+++ ProgettoEsame_GervasioFrick/src/main/java/main/ProgettoEsameGervasioFrickApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoEsameGervasioFrickApplication {
+
+	public static void main(String[] args) {//assad
+		SpringApplication.run(ProgettoEsameGervasioFrickApplication.class, args);
+	}
+
+}
--- src/services/Utilities.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-public class Utilities {
-	
-	public static final int NULLINT=-1;
-	public static final String NULLSTRING="";
-	public static final double NULLDOUBLE=0.0;
-	
-}
